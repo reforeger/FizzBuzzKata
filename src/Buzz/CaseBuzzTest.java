f0ab07de9938f0131da549d01c39b032eb991348
reforeger <daniel.mihol@gmail.com>
2021-11-09 10:46:46 +0200

    commit test

---
+++ src/Buzz/CaseBuzzTest.java
@@ -0,0 +1,45 @@
+package Buzz;
+
+
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CaseBuzzTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+    @Test
+    public void getResultShouldReturnFizzIfTheNumberIsDividableBy3() {
+        Assert.assertEquals("fizz", FizzBuzz.getResult(3));
+    }
+ 
+    @Test
+    public void getResultShouldReturnBuzzIfTheNumberIsDividableBy5() {
+        Assert.assertEquals("buzz", FizzBuzz.getResult(5));
+        Assert.assertEquals("buzz", FizzBuzz.getResult(10));
+    }
+ 
+    @Test
+    public void getResultShouldReturnBuzzIfTheNumberIsDividableBy15() {
+        Assert.assertEquals("fizzbuzz", FizzBuzz.getResult(15));
+        Assert.assertEquals("fizzbuzz", FizzBuzz.getResult(30));
+    }
+ 
+    @Test
+    public void getResultShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled() {
+        Assert.assertEquals("1", FizzBuzz.getResult(1));
+        Assert.assertEquals("2", FizzBuzz.getResult(2));
+        Assert.assertEquals("4", FizzBuzz.getResult(4));
+    }
+	
+
+}
